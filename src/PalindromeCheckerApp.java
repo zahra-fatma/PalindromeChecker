db41b25028dfb46db25dab842a18f1d63ce0a3a7
zahra-fatma <zahrafatma548@gmail.com>
2026-03-05 22:34:06 +0530

    UC5 - Stack based palindrome checker implemented

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -5,26 +7,23 @@ public class PalindromeCheckerApp {
         
         String word = "madam";
 
-        
-        char[] characters = word.toCharArray();
+        Stack<Character> stack = new Stack<>();
 
         
-        int start = 0;
-        int end = characters.length - 1;
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
 
         boolean isPalindrome = true;
 
-       
-        while (start < end) {
-            if (characters[start] != characters[end]) {
+      
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
-       
         if (isPalindrome) {
             System.out.println("The word \"" + word + "\" is a Palindrome.");
         } else {
