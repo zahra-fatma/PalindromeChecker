ff09e404df2917a655cec9a98e4595f9cde6cabf
zahra-fatma <zahrafatma548@gmail.com>
2026-03-05 20:37:29 +0530

    UC1 - Basic palindrome checker

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+       
+        System.out.println("  Welcome to Palindrome Checker  ");
+   
+        System.out.println("Version 1.0 - Application Started");
+    }
+}
